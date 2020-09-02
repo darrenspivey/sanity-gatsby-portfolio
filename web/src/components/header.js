20fefaffe32bf2d11cfba64a8aa0575ea314ae3f
darrenspivey <hello@darrenspivey.co.uk>
2020-09-02 16:46:03 +0100

    logo update

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -8,12 +8,15 @@ import styles from './header.module.css'
 
 const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
   <div className={styles.root}>
-    <Link to='/'><img src={logo} alt="Logo"/></Link>
+ 
     <div className={styles.wrapper}>
       <div className={styles.branding}>
-     
-        <Link to='/'> {siteTitle}</Link>
+
+
+      <div className={styles.logo}>
+      <img src={logo} alt="Logo"/>
       </div>
+          </div>
          <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
         <Icon symbol='hamburger' />
       </button>
